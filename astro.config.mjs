070f371cf6f50e0bd1d406a56cbef1e016d092d1
0riceisnice0-hash <0riceisnice0@gmail.com>
2026-01-17 20:10:16 -0500

    Update astro.config.mjs with new configurations

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,7 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://0riceisnice0-hash.github.io',
-  base: '/marketing',
+  site: 'https://hydronmarketing.com',
   integrations: [
     tailwind(),
     sitemap()
